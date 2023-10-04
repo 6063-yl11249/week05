4f71c35aef36ecdcb6f0b4c2c200a267f2bcaa78
Yuanqing <yl11249@nyu.edu>
2023-10-04 16:30:52 -0400

    Array

--- data-structures/sketch.js
+++ data-structures/sketch.js
@@ -1,27 +1,36 @@
-let xPos = 0;
-let yPos = 150;
+let xPos = [];
+let yPos = [];
+let xVel = [];
+let cDiam = [];
 
-let xVel = 2;
-
-let cDiam = 40;
+let numCircles = 10;
 
 function setup() {
   createCanvas(800, 600);
+
+  for (let i = 0; i < numCircles; i += 1) {
+    xPos.push(0);
+    yPos.push(random(20, 550));
+    xVel.push(random(5, 15));
+    cDiam.push(random(10, 45));
+  }
 }
 
 function draw() {
   background("lightblue");
   fill(220, 10, 100);
 
-  // draw circle
-  ellipse(xPos, yPos, cDiam, cDiam);
+  for (let i = 0; i < numCircles; i += 1) {
+    ellipse(xPos[i], yPos[i], cDiam[i], cDiam[i]);
+    // draw circle
 
-  // update pos
-  xPos = xPos + xVel;
+    // update pos
+    xPos[i] = xPos[i] + xVel[i];
 
-  // if reset is needed: pick random diameter and reset x
-  if (xPos > width + cDiam) {
-    cDiam = random(20, 75);
-    xPos = 0 - cDiam;
+    // if reset is needed: pick random diameter and reset x
+    if (xPos[i] > width + cDiam[i]) {
+      cDiam[i] = random(20, 75);
+      xPos[i] = 0 - cDiam[i];
+    }
   }
 }
